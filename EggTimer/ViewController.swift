b2e3ccddf98933d6b9ba34493f0b9864e0219ff7
Luiz Doreto <luiz.doreto@icloud.com>
2020-11-02 13:07:07 -0300

    Setting up timer and a UIprogressView

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,9 +7,47 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var progressView: UIProgressView!
+    var player: AVAudioPlayer!
+    
+    let eggTimes = ["Soft": 3, "Medium": 4, "Hard": 7]
+    var timer = Timer()
+    var totalTime = 0
+    var secondsPassed = 0
 
-
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        // reset previous timer countdown
+        timer.invalidate()
+        let hardness : String = sender.currentTitle!
+        totalTime = eggTimes[hardness]!
+        
+        // reset previous states
+        progressView.progress = 0.0
+        secondsPassed = 0
+        titleLabel.text = hardness
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateTimer() {
+        if secondsPassed < totalTime {
+            secondsPassed += 1
+            progressView.progress = Float(secondsPassed) / Float(totalTime)
+        } else {
+            timer.invalidate()
+            titleLabel.text = "Done!"
+            playSound()
+        }
+     }
+    
+    func playSound() {
+        let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
 }
